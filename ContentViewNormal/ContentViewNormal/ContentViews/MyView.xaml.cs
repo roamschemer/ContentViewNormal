a25138e1f9a5ad718db4e89404815e132459d67d
放浪 軍師 <roamschemer@gmail.com>
2019-12-08 22:43:54 +0900

    プロジェクト ファイルを追加します。

---
+++ ContentViewNormal/ContentViewNormal/ContentViews/MyView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ContentViewNormal.ContentViews
+{
+
+    public partial class MyView : ContentView
+    {
+        public string TextData1 { get; set; }
+        public string TextData2 { get; set; }
+        public string TextData3 { get; set; }
+        public MyView()
+        {
+            InitializeComponent();
+        }
+        void OnButton1_Clicked(object sender, EventArgs args)
+        {
+            label1.Text = TextData1;
+
+        }
+
+        void OnButton2_Clicked(object sender, EventArgs args)
+        {
+            label1.Text = TextData2;
+        }
+
+        void OnButton3_Clicked(object sender, EventArgs args)
+        {
+            label1.Text = TextData3;
+        }
+    }
+}
---
+++ ContentViewNormal/ContentViewNormal/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace ContentViewNormal.Views
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
